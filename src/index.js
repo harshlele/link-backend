e65cef5e5d048d3747e7aaaed602e72cfaadb5a8
Harshal Lele <harshallele97@gmail.com>
2020-09-05 10:38:20 +0530

    added check to see if a link has already been added

--- src/index.js
+++ src/index.js
@@ -13,27 +13,51 @@ const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${proce
 const client = new MongoClient(url,{useUnifiedTopology: true});
 
 async function storeLink(data){
-  
-  await client.connect();
-  
+  if(!client.isConnected()) await client.connect();
   const links = client.db("links").collection("link");
   const result = await links.insertOne(data);
   return result;
-  
 }
 
-app.post("/shorten",(req,res) => {
-  let data = {shortened: (Math.random() +1).toString(36).substr(2, 5), link: req.body.url};
-  const result = storeLink(data);
+async function getLink(link){
+  if(!client.isConnected()) await client.connect();
+  const links = client.db("links").collection("link");
+  const query = {link};
+  const document = await links.findOne(query);
+  return document;
+}
+
+app.post("/shorten",async (req,res) => {
+
+  let response = {};
+  const doc = await getLink(req.body.url);
+  if(doc == null){
+    response = {shortened: (Math.random() +1).toString(36).substr(2, 5), link: req.body.url};
+    const result = await storeLink(response);
+    response.success = result.insertedCount;
+  }
+  else{
+    response = {...doc};
+    response.success = "1";
+  }
+  delete response._id;
+
   res.status(200);
   res.setHeader('Content-Type','application/json');
-  res.end(JSON.stringify(data));
+  res.send(JSON.stringify(response));
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
+
 process.on('SIGTERM',async () => {
   if(client.isConnected()) await client.close();
 });
+
+process.on('uncaughtException',(err,origin) => {
+  console.error("Error: ",err);
+  process.exitCode(1);
+})
+
